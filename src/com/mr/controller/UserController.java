2f8230db69000feb147120866fe2cc9d3f17dae0
李好杰 <lihaojie@intra.nsfocus.com>
2023-03-30 15:20:54 +0800

    ssss

--- src/com/mr/controller/UserController.java
+++ src/com/mr/controller/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
     public String toUpd(UsersBean usersBean) {
         userDao.updUser(usersBean);
         return "forward:getAllUser";
-
+///sdsd///
     }
 
     @RequestMapping("/delUser")
